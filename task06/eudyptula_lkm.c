c222431b17ab11be37d1e55a446181111f6f84ff
n2 h9 <n2h9z4@gmail.com>
2023-02-27 21:26:15 +0100

    task06: misc_read implementation

--- task06/eudyptula_lkm.c
+++ task06/eudyptula_lkm.c
@@ -12,27 +12,44 @@ MODULE_LICENSE("Dual MIT/GPL");
 MODULE_AUTHOR("n2h9");
 MODULE_DESCRIPTION("task01");
 
+// interal driver data
+struct device_ctx {
+	char *id;
+	struct device *dev;
+};
+
+static struct device_ctx ctx = {
+	.id = "n2h9"
+};
+
 static int misc_open(struct inode *inode, struct file *f)
 {
-	// @TODO
-	return 0;
+
+	dev_info(ctx.dev, "open method called \n");
+	return nonseekable_open(inode, f);
 }
 
-static ssize_t misc_read(struct file *f, char __user *ubuff, size_t count, loff_t *offset)
+static ssize_t misc_read(struct file *f, char __user *ubuf, size_t count, loff_t *offset)
 {
-	// @TODO
-	return 0;
+	dev_info(ctx.dev, "read method called \n");
+	if(copy_to_user(ubuf, ctx.id, sizeof(ctx.id))) {
+		dev_warn(ctx.dev, "copy_to_user failed");
+		return -EFAULT;
+	}
+
+	return sizeof(ctx.id);
 }
 
 static ssize_t misc_write(struct file *f, const char __user *ubuff, size_t count, loff_t *offset)
 {
 	// @TODO
+	dev_info(ctx.dev, "write method called @TODO HERE \n");
 	return 0;
 }
 
 static int misc_release(struct inode *inode, struct file *f)
 {
-	// @TODO
+	dev_info(ctx.dev, "release method called \n");
 	return 0;
 }
 
@@ -41,6 +58,7 @@ static const struct file_operations misc_fops = {
 	.read = misc_read,
 	.write = misc_write,
 	.release = misc_release,
+	.llseek = no_llseek,
 };
 
 static struct miscdevice n2h9_miscdev = {
@@ -54,18 +72,28 @@ static int __init module_init_func(void)
 {
 	int ret;
 
-	pr_debug("Hello World!\n");
 	ret = misc_register(&n2h9_miscdev);
 	if (ret != 0) {
 		pr_err("misc device registration error, aborting\n");
 		return ret;
 	}
 
-	return 0;	/* success */
+	ctx.dev = n2h9_miscdev.this_device;
+	
+	dev_info(
+		ctx.dev, 
+		"misc device driver registered minor number = %d", 
+		n2h9_miscdev.minor
+	);
+
+	return 0; /* success */
 }
 
 static void __exit module_exit_func(void)
 {
+	ctx.dev = NULL;
+	misc_deregister(&n2h9_miscdev);
+	pr_info("misc device deregistered\n");
 }
 
 
